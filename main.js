f814c93e1f9761387519338ea1060ae5dfd64063
like2300 <125821231+like2300@users.noreply.github.com>
2023-07-29 21:31:36 +0200

    version 3 prod

---
+++ main.js
@@ -0,0 +1,10 @@
+
+setInterval(function loa(){
+
+        var load = document.getElementById("loading");
+        
+        
+            load.style.display="none";
+        
+        },5000)
+        
